fe502d197b0e656a72849bb5b82f17184ef86281
Antonio Carlos <136139831+Carllosdodev@users.noreply.github.com>
2024-03-14 02:36:31 -0300

    finish
    obs: falta responsividade, algumas adequações, para melhorias e animações

--- script.js
+++ script.js
@@ -1,48 +1,85 @@
-function criptografar(texto) {
-    return texto.replace(/e/g, 'enter')
-                .replace(/i/g, 'imes')
-                .replace(/a/g, 'ai')
-                .replace(/o/g, 'ober')
-                .replace(/u/g, 'ufat');
+
+function exibirResultado(tag, texto) {
+    var campo = document.querySelector(tag);
+    campo.innerHTML = texto;
 }
 
-function descriptografar(texto) {
-    return texto.replace(/enter/g, 'e')
-                .replace(/imes/g, 'i')
-                .replace(/ai/g, 'a')
-                .replace(/ober/g, 'o')
-                .replace(/ufat/g, 'u');
+function criptografar() {
+    var textoEntrada = document.getElementById("campoTexto").value;
+    var resultadoCripto = textoEntrada.replace(/e/g, "enter").replace(/i/g, "imes").replace(/a/g, "ai").replace(/o/g, "ober").replace(/u/g,"ufat");
+
+    if (textoEntrada == 0) {
+        let textoIndicacao = "Nenhuma mensagem encontrada"
+        let textoIndicacao2 = "Digite um texto que você deseja criptografar ou descriptografar"
+        exibirResultado('#texto', resultadoCripto);
+        exibirResultado('#texto2', textoIndicacao2);
+        imagem('none');
+        document.getElementById('botaoCopiar').style.display = 'none';
+    } else {
+        exibirResultado('#texto', resultadoCripto);
+        exibirResultado('#texto2', "" );
+        imagem('block')
+        document.getElementById('botaoCopiar').style.display = 'block';
+    }
+    limparCampo();
+}
+
+function descriptografar() {
+    var textoEntrada = document.getElementById('campoTexto').value;
+    var resultadoDescripto = textoEntrada.replace(/enter/g, "e").replace(/imes/g, "i").replace(/ai/g, "a").replace(/ober/g, "o").replace(/ufat/g, "u");
+    if (textoEntrada == '') {
+        var textoIndicacao = "Nenhuma mensagem encontrada";
+        var textoIndicacao2 = "Digite um texto que você deseja criptografar ou descriptografar.";
+        exibirResultado('#texto', textoIndicacao);
+        exibirResultado('#texto2', textoIndicacao2);
+        imagem('block');
+        document.getElementById('botaoCopiar').style.display = 'block';
+    } else {
+        exibirResultado('#texto', resultadoDescripto);
+        exibirResultado('#texto2', "");
+        imagem('none');
+        document.getElementById('botaoCopiar').style.display = 'none';
+        
+    }
+
+    limparCampo();
 }
 
 function limparCampo() {
-    document.getElementById('campoTexto').value = '';
-    document.getElementById('texto').innerText = '';
-    document.getElementById('imagemPerson').src = './assets/High quality products 1 1.svg';
+    var texto = document.getElementById('campoTexto');
+    texto.value = '';
 }
 
-document.getElementById('botaoCriptografar').addEventListener('click', function() {
-    var textoOriginal = document.getElementById('campoTexto').value;
-    var textoCriptografado = criptografar(textoOriginal);
-    document.getElementById('texto').innerText = textoCriptografado;
-    document.getElementById('texto2').innerText = '';
-    document.getElementById('imagemPerson').style.display = 'none';
-});
+function copiarResultado() {
+    var textoResultado = document.getElementById('texto').textContent;
 
-document.getElementById('botaoDescriptografar').addEventListener('click', function() {
-    var textoCriptografado = document.getElementById('campoTexto').value;
-    var textoDescriptografado = descriptografar(textoCriptografado);
-    document.getElementById('texto').innerText = textoDescriptografado;
-    document.getElementById('texto2').innerText = '';
-    document.getElementById('imagemPerson').style.display = 'block';
-});
+    if (textoResultado) {
+        var textArea = document.createElement('textarea');
+        textArea.value = textoResultado;
+        textArea.style.position = 'fixed';
+        document.body.appendChild(textArea);
+    
 
-document.getElementById('botaoCopiar').addEventListener('click', function() {
-    var textoResultado = document.getElementById('texto').innerText;
-    navigator.clipboard.writeText(textoResultado).then(function() {
-        
-    }, function(err) {
+    textArea.select();
+    
+    try {
+        document.execCommand('copy');
+        console.log('Texto copiado: ' + textoResultado);
+    } catch (err) {
         console.error('Erro ao copiar texto: ', err);
-    });
-});
+    }
 
-document.querySelector('button[onclick="limparCampo()"]').addEventListener('click', limparCampo);
+        document.body.removeChild(textArea);
+
+        document.getElementById('texto').innerHTML = '';
+
+        location.reload(); 
+        } else {
+        console.log('Nenhum texto para copiar.');
+        }
+}
+
+document.getElementById('botaoCriptografar').addEventListener('click', criptografar);
+document.getElementById('botaoDescriptografar').addEventListener('click', descriptografar);
+document.getElementById('botaoCopiar').addEventListener('click', copiarResultado);
+document.getElementById('limparCampo').addEventListener('click', limparCampo);
